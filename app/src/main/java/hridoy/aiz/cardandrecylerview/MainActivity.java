2892488b34f9f471722707c2aba82ff121769f4e
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-08-22 18:33:34 +0600

    commit3

--- app/src/main/java/hridoy/aiz/cardandrecylerview/MainActivity.java
+++ app/src/main/java/hridoy/aiz/cardandrecylerview/MainActivity.java
@@ -1,14 +1,50 @@
 package hridoy.aiz.cardandrecylerview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter myAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    List<Person> personList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        personList = new ArrayList<Person>();
+        personList.add(new Person("John", "Adams", "bus"));
+        personList.add(new Person("George", "Washington", "plane"));
+        personList.add(new Person("Hamilton", "Masakadza", "bus"));
+        personList.add(new Person("John", "Adams", "bus"));
+        personList.add(new Person("George", "Washington", "plane"));
+        personList.add(new Person("Hamilton", "Masakadza", "bus"));
+        personList.add(new Person("John", "Adams", "bus"));
+        personList.add(new Person("George", "Washington", "plane"));
+        personList.add(new Person("Hamilton", "Masakadza", "bus"));
+        personList.add(new Person("John", "Adams", "bus"));
+        personList.add(new Person("George", "Washington", "plane"));
+        personList.add(new Person("Hamilton", "Masakadza", "bus"));
+        personList.add(new Person("John", "Adams", "bus"));
+        personList.add(new Person("George", "Washington", "plane"));
+        personList.add(new Person("Hamilton", "Masakadza", "bus"));
+
+        myAdapter = new PersonAdapter(this, personList);
+        recyclerView.setAdapter(myAdapter);
     }
 }
